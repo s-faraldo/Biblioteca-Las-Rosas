12675e38675d94674a2e39c8f1934c641f8886a0
EmiSan <s.faraldo122@gmail.com>
2020-06-17 18:54:35 -0300

    10 no 1

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		Biblioteca lasRosas = new Biblioteca(1);
+		Biblioteca lasRosas = new Biblioteca(10);
 		String dewey ="";
 		String codigo ="";
 		String tipo ="";
